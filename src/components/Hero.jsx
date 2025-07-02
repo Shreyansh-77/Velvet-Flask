4492b63a7467f747daa7a86cea825b1925be886e
Shreyansh-77 <shreyanshsoni9136@gmail.com>
2025-07-03 03:09:39 +0530

    Added glass animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,14 @@
 import gsap from 'gsap';
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
-
+import {useRef} from "react";
+import { useMediaQuery} from "react-responsive";
 
 const Hero = () => {
+    const videoRef = useRef();
+    const isMobile = useMediaQuery({maxWidth: 767});
+
+
     useGSAP(() =>
     {
         const heroSplit = new SplitText('.title', {type: 'chars, words'});
@@ -37,9 +42,28 @@ const Hero = () => {
         })
             .to('.right-leaf', {y:200},0 )
             .to('.left-leaf', {y:-200},0 )
+        const startValue = isMobile ? 'top 60%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger:'video',
+                start:startValue,
+                end:endValue,
+                scrub:true,
+                pin:true,
+            }
+        })
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration
+            })
+        }
     },[])
 
     return (
+        <>
         <section id="hero" className='noisy'>
             <h1 className='title'>MOJITO</h1>
 
@@ -66,6 +90,17 @@ const Hero = () => {
                 </div>
             </div>
         </section>
+
+        <div className='video absolute inset-0'>
+            <video
+                ref={videoRef}
+                src="/videos/input.mp4"
+                muted
+                playsInline
+                preload
+            />
+        </div>
+        </>
     )
 }
 export default Hero
